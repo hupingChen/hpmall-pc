f8070027dbfad4aaa8947ba5350fe29d2b3da6fd
Huping Chen <18327184833@163.com>
2025-03-25 07:40:53 +0800

    初次提交，搭建基本框架。

---
+++ hpmall-protal/src/main/java/com/hpedu/mall/hpmallprotal/HpmallProtalApplication.java
@@ -0,0 +1,13 @@
+package com.hpedu.mall.hpmallprotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpmallProtalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpmallProtalApplication.class, args);
+    }
+
+}
---
+++ hpmall-protal/src/test/java/com/hpedu/mall/hpmallprotal/HpmallProtalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpedu.mall.hpmallprotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpmallProtalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
