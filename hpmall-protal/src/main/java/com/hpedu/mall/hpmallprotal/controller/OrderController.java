4e09dccdfd43b5811385901751b3760982fe6af6
Huping Chen <18327184833@163.com>
2025-03-25 10:48:54 +0800

    初步实现了负载均衡的雏形

--- hpmall-protal/src/main/java/com/hpedu/mall/hpmallprotal/controller/OrderController.java
+++ hpmall-protal/src/main/java/com/hpedu/mall/hpmallprotal/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.hpedu.mall.hpmallprotal.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -29,11 +33,20 @@ public class OrderController {
     private RestTemplate restTemplate;
 
 
+
+
+    private String getGoodsServer(){
+        String serverList="http://localhost:9090/goods,http://localhost:9093/goods";
+        String servers[]=serverList.split(",");
+        Random random=new Random();
+        return servers[random.nextInt(servers.length)];
+    }
+
+
     @GetMapping
     public String order(){
         log.info("began in order");
-
-        String goodsInfo = restTemplate.getForObject("http://localhost:9090/goods", String.class);
+        String goodsInfo = restTemplate.getForObject(getGoodsServer(), String.class);
         String promotionInfo = restTemplate.getForObject("http://localhost:9091/promotion", String.class);
         MultiValueMap<String,Object> param = new LinkedMultiValueMap<>();
         param.add("goodsInfo",goodsInfo);
--- ms-service/goods-service/src/main/java/com/hpedu/mall/goodsservice/controller/GoodsService.java
+++ ms-service/goods-service/src/main/java/com/hpedu/mall/goodsservice/controller/GoodsService.java
@@ -1,10 +1,16 @@
 package com.hpedu.mall.goodsservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class GoodsService {
+
+    @Value("${server.port}")
+    private String port;
 
 
     /**
@@ -13,9 +19,9 @@ public class GoodsService {
      * @date 2025/3/25 8:51
      * @return String
      */
-
     @GetMapping("/goods")
     public String getGoodsById(){
+        log.info("收到请求端口为：{}的请求",port);
         return "返回商品信息";
     }
 
